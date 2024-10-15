6be23c8abec1e554f6bf9b077e9c3411181d78f8
Andriy bc2024-4 <andriykramar465@gmail.com>
2024-10-15 23:39:53 +0300

    Додано обробку http-метода GET

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { program } = require("commander");
 const http = require("http");
-//const
+const fsp = require("node:fs/promises");
+const path = require('node:path');
+
 
 function preparing() {
     // опис параметрів програми
@@ -25,17 +27,37 @@ function preparing() {
 	return options;	
 }
 
-function requestListener(req, res) {
+const options = preparing();
 
-    res.writeHead("200");
-    res.end("Hello!");
+function getPicture(cashe_way, code) {
+    const way = path.normalize(`${cashe_way}${code}.png`);
+
+    return fsp.readFile(way, "binary");
 }
 
 
+function requestListener(req, res) {
+    const code_number = req.url;
+
+    switch (req.method) {
+    case "GET":
+        getPicture(options.cashe, code_number)
+        .then(
+            (result) => {
+                res.writeHead(200, {"Content-Type": "image/jpeg"});
+                res.end(result);
+            })
+        .catch((error) => {
+                res.writeHead(404);
+            });
+        break;
+    default:
+        res.writeHead(405);
+    }
+}
 
 
 function main() {
-    options = preparing();
     server = http.createServer(requestListener);
 
     const host = options.host;
@@ -44,7 +66,6 @@ function main() {
     server.listen(port, host, () => {
         console.log(`Server started, url -> ${options.host}:${options.port}`);
     })
-
 }
 
 main();
